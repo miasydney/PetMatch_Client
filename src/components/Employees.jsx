464fce4ba3c5c1bc5bfce49b1459363631228a1a
work <miabenn233@gmail.com>
2023-02-14 11:59:23 +1000

    Display list of all user accounts on ManageEmployees page. Add delete functionality to delete user accounts

--- src/components/Employees.jsx
+++ src/components/Employees.jsx
@@ -1,18 +1,63 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
 import { Button } from 'react-bootstrap'
 import NavBar from './NavBar'
+import axios from 'axios'
+
 
 const Employees = () => {
-    return (
-      <>
-        <NavBar />
-        <div>Manage Employees</div>
-        <Link to="/add-employee">
-          <Button>Add Employee</Button>
-        </Link>
-      </>
-    );
+  const [employees, setEmployees] = useState([]);
+
+  // retrieve employees from API
+  useEffect(() => {
+    axios
+      .get("/users")
+        .then((res) => {
+            console.log(res)
+            setEmployees(res.data)
+        })
+      .catch((err) => console.log(err));
+  }, []);
+
+  // delete employee by id
+  const deleteEmployee = (id) => {
+    axios
+      .delete(`/users/${id}`)
+      .then((res) => {
+        console.log(res);
+        setEmployees((prev) => {
+          return prev.filter((employee) => employee.id !== id);
+        });
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <>
+      <NavBar />
+      <h1>Manage Employees</h1>
+      <Link to="/add-employee">
+        <Button variant="outline-success">Add Employee</Button>
+      </Link>
+
+      <h3>All Employees</h3>
+      <ul>
+        {employees.map((employee) => (
+          <li key={employee._id}>
+            <span>
+              {employee.username} ({employee.roles})
+            </span>
+            <Button
+              variant="outline-danger"
+              onClick={() => deleteEmployee(employee._id)}
+            >
+              Delete Account
+            </Button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default Employees
