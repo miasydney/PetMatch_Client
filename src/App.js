2fcd1a9b752821c7b04cdc04b8387effff8b30a7
work <miabenn233@gmail.com>
2023-02-20 20:35:11 +1000

    Move router logic to App component, access store to show login page unless user signed in

--- src/App.js
+++ src/App.js
@@ -1,49 +1,75 @@
 import { useEffect, useReducer } from 'react';
-import { Navbar } from 'react-bootstrap';
 import './App.css';
 import Dashboard from './components/Dashboard';
 import Login from './components/Login';
 import globalReducer from './reducers/globalReducer';
 import { GlobalContext } from './utils/globalStateContext';
+import NavBar from './components/NavBar';
+import { Outlet, Route } from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import NotFound from './components/NotFound';
+import AddAnimal from './components/AddAnimal';
+import AnimalList from './components/AnimalList';
+import Employees from './components/Employees';
+import AddEmployee from './components/AddEmployee';
 
 function App() {
-
   // set initial app states
   const initialState = {
     loggedInUserName: "",
     token: "",
     userRole: "",
-  }
-  const [store, dispatch] = useReducer(globalReducer, initialState)
+  };
+  const [store, dispatch] = useReducer(globalReducer, initialState);
 
   // Maintain logged in status after browser refresh
   useEffect(() => {
-    const username = localStorage.getItem('username')
-    const token = localStorage.getItem('token')
-    const userRole = localStorage.getItem('userRole')
+    const username = localStorage.getItem("username");
+    const token = localStorage.getItem("token");
+    const userRole = localStorage.getItem("userRole");
     if (username && token && userRole) {
       dispatch({
-        type: 'setLoggedInUserName',
-        data: username
-      })
+        type: "setLoggedInUserName",
+        data: username,
+      });
       dispatch({
         type: "setToken",
-        data: token
+        data: token,
       });
       dispatch({
         type: "setUserRole",
-        data: userRole
+        data: userRole,
       });
     }
-  }, [])
+  }, []);
+
+  // create router
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={store.token ? <MainPage /> : <Login />} errorElement={<NotFound />}>
+        <Route path="add-animal" element={<AddAnimal />} />
+        <Route path="/animals" element={<AnimalList />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/employees" element={<Employees />} />
+        <Route path="/add-employee" element={<AddEmployee />} />
+      </Route>
+    )
+  );
+
+  // Main page component
+  function MainPage() {
+    return (
+      <>
+        <NavBar />
+        <Outlet />
+      </>
+    );
+  }
 
   return (
     <div className="App">
       <GlobalContext.Provider value={{ store, dispatch }}>
-        <h1>Home page</h1>
-        <Login />
-        <Dashboard />
-        <Navbar />
+        <RouterProvider router={router} />
       </GlobalContext.Provider>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,19 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import AddAnimal from './components/AddAnimal';
-import AnimalList from './components/AnimalList';
-import Dashboard from './components/Dashboard';
-import Employees from './components/Employees';
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AddEmployee from './components/AddEmployee';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <App />
+    {/* <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/add-animal" element={<AddAnimal />} />
@@ -23,7 +18,7 @@ root.render(
         <Route path="/employees" element={<Employees />} />
         <Route path="/add-employee" element={<AddEmployee />} />
       </Routes>
-    </BrowserRouter>
+    </BrowserRouter> */}
   </React.StrictMode>
 );
 
