082393da66b873bffa0c9e312c88229d8c89d44c
work <miabenn233@gmail.com>
2023-02-20 23:47:07 +1000

    Add edit employee modal and functionality and test edit user

--- src/components/Employees.jsx
+++ src/components/Employees.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
-import { Button } from 'react-bootstrap'
+import { Button, Form, Modal } from 'react-bootstrap'
 import axios from 'axios'
 
 
@@ -17,6 +17,40 @@ const Employees = () => {
         })
       .catch((err) => console.log(err));
   }, []);
+
+  // Edit employee
+  const [updatedEmployee, setUpdatedEmployee] = useState({});
+  const [showEditModal, setShowEditModal] = useState(false);
+
+  const handleCloseEditModal = () => setShowEditModal(false);
+  const handleShowEditModal = () => setShowEditModal(true);
+
+  const updateEmployee = (employee) => {
+    setUpdatedEmployee(employee);
+    handleShowEditModal();
+  };
+
+  const handleFormChange = (e) => {
+    const { name, value } = e.target;
+
+    setUpdatedEmployee((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+
+  const saveUpdatedEmployee = () => {
+    console.log(updatedEmployee)
+    axios
+      .patch(`/users/${updatedEmployee.id}`, updatedEmployee)
+      .then(() => {
+        handleCloseEditModal();
+        window.location.reload();
+      })
+      .catch((err) => console.log(err));
+  };
 
   // delete employee by id
   const deleteEmployee = (id) => {
@@ -35,6 +69,46 @@ const Employees = () => {
 
   return (
     <>
+      {/* Edit employee modal */}
+      <Modal show={showEditModal} onHide={handleCloseEditModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Edit Employee</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="formBasicUsername">
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter username"
+                name="username"
+                value={updatedEmployee.username}
+                onChange={handleFormChange}
+              />
+            </Form.Group>
+            <Form.Group controlId="formBasicIsAdmin">
+              <Form.Label>Select Role</Form.Label>
+              <Form.Select
+                name="isAdmin"
+                value={updatedEmployee.isAdmin}
+                onChange={handleFormChange}
+              >
+                <option value={true}>Admin</option>
+                <option value={false}>Employee</option>
+              </Form.Select>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseEditModal}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={saveUpdatedEmployee}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
       <h1>Manage Employees</h1>
       <Link to="/add-employee">
         <Button variant="outline-success">Add Employee</Button>
@@ -46,8 +120,15 @@ const Employees = () => {
           {employees.map((employee) => (
             <li key={employee._id}>
               <span>
-                {employee.username} ({employee.isAdmin ? ("Admin") : ("Employee")})
+                {employee.username} ({employee.isAdmin ? "Admin" : "Employee"})
               </span>
+              <Button
+                variant="outline-info"
+                onClick={() => updateEmployee(employee)}
+                style={{ marginRight: "1rem" }}
+              >
+                Edit
+              </Button>
               <Button
                 variant="outline-danger"
                 onClick={() => deleteEmployee(employee._id)}
