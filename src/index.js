745ab76a616f458fd7a7f4d299e6122a9efd031f
work <miabenn233@gmail.com>
2023-03-03 12:20:43 +1000

    Add baseURL

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.min.css";
 
+axios.defaults.baseURL = "https://petmatch-production-6ea7.up.railway.app/";
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
